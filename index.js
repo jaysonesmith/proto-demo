4730208c3637b223a58e2e4d0306469a27916935
jaysonesmith <gh@nes.33mail.com>
2020-02-27 08:54:14 -0700

    Bar works, but not foo

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import * as protoLoader from "@grpc/proto-loader"
-import path from "path"
-
-var includePath = path.join(__dirname, "./proto/demo_services")
-console.log("includePath:", includePath)
 
 const opts = {
-  includeDirs: [includePath]
+  includeDirs: ['./proto']
 }
 
 const loadServiceProtos = (service) => {
-  let packageDefinition = protoLoader.loadSync(`${service}_service.proto`, opts)
+  let packageDefinition = protoLoader.loadSync(`demo_services/${service}/${service}_service.proto`, opts)
 
   console.log("packageDefinition", packageDefinition)
 
